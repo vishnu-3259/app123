dc80be564812d692611e7b983ec3c7a27cc32496
vishnu <vishnupardha1040@gmail.com>
2022-06-20 21:22:51 +0530

    Updates

---
+++ precache-manifest.0397f62601bd8e6432384436b75a7401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "127f2b8077d40aab490051586f0d91f6",
+    "url": "/app123/index.html"
+  },
+  {
+    "revision": "f85b2be78d82aa48c01f",
+    "url": "/app123/static/css/main.7d44d451.chunk.css"
+  },
+  {
+    "revision": "a336aeee0956617dfa53",
+    "url": "/app123/static/js/2.f442603d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app123/static/js/2.f442603d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85b2be78d82aa48c01f",
+    "url": "/app123/static/js/main.4300d7f5.chunk.js"
+  },
+  {
+    "revision": "0a83c50509420aa8199d",
+    "url": "/app123/static/js/runtime-main.8ba1a520.js"
+  }
+]);
