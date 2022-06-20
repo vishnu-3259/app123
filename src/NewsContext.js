8f35d3a8cb41c48eb25e460e2454e73b6a91e62d
vishnu <vishnupardha1040@gmail.com>
2022-06-20 21:27:29 +0530

    ff

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -10,7 +10,7 @@ export const NewsContextProvider = (props) => {
   useEffect(() => {
     axios
       .get(
-        `http://newsapi.org/v2/everything?q=rich&from=2020-07-19&sortBy=publishedAt&apiKey=${apiKey}`
+        `https://newsapi.org/v2/everything?q=tesla&from=2022-04-28&sortBy=publishedAt&apiKey=54b45f51b31b414f80e3e2d0395311c8=${apiKey}`
       )
       .then((response) => setData(response.data))
       .catch((error) => console.log(error));
